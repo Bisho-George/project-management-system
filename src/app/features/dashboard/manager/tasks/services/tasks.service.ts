060383089ccb4912332520599edce420479d09db
romany <romanyghttas332@gmail.com>
2024-12-16 19:01:58 +0200

    [feat] task module add fillter data

--- src/app/features/dashboard/manager/tasks/services/tasks.service.ts
+++ src/app/features/dashboard/manager/tasks/services/tasks.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -15,7 +16,15 @@ constructor(private _http: HttpClient) { }
         title: params?.title || '',
         pageNumber: params?.pageNumber || 1,
         pageSize: params?.pageSize || 5,
+        status:params?.status 
       }
     });
   }
+  addTask (task: FormGroup) {
+      return this._http.post('Task', task);
+    }
+    deleteTask (id: number) {
+      return this._http.delete(`Task/${id}`);
+    }
+
 }
