e7fcaae180f5777c87afcb85e0dbea2f2cd1d75d
romany <romanyghttas332@gmail.com>
2024-12-19 00:29:57 +0200

    [feat] users module : create users params interface

--- src/app/features/dashboard/manager/users/services/users.service.ts
+++ src/app/features/dashboard/manager/users/services/users.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IUsersParams } from '../interfaces/users-params';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class UsersService {
 
  constructor(private _http: HttpClient) { }
 
-  getUsers(params?: any): Observable<any> {
+  getUsers(params?: IUsersParams): Observable<any> {
     return this._http.get('Users/', {
       params: {
         userName: params?.userName || '',
