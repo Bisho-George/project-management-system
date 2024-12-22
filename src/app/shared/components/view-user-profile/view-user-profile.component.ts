aaa087084b113f3d4ba161340ae8fb3df36ce31f
romany <romanyghttas332@gmail.com>
2024-12-22 03:05:54 +0200

    [feat] shared module : delete block button in view user

--- src/app/shared/components/view-user-profile/view-user-profile.component.ts
+++ src/app/shared/components/view-user-profile/view-user-profile.component.ts
@@ -14,34 +14,9 @@ export class ViewUserProfileComponent {
   constructor(
     public dialogRef: MatDialogRef<ViewUserProfileComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private _UsersService: UsersService,
-    private toast: ToastrService
   ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
-  }
-  getUsers() {
-      let userParams = {
-        pageNumber: 1,
-        pageSize: 9999,
-      };
-      this._UsersService.getUsers(userParams).subscribe({
-        next: (res: IDataResponse<IUser>) => {},
-        error: (err) => {}
-      });
-    }
-  openBlockDialog(id: number): void {
-    this._UsersService.onActivateUser(id).subscribe({
-      next: (res) => {
-      },
-      error: (err) => {
-        this.toast.error(err.error.message, 'error');
-      },
-      complete: () => {
-        this.toast.success('Activity updated successfully');
-        this.getUsers()
-      },
-    });
   }
 }
